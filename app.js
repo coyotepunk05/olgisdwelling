122a012a9567dc8a8992844cbc69ea8c145e84c8
coyotepunk05 <coyote582005@gmail.com>
2024-12-03 23:39:07 -0800

    first working setup

---
+++ app.js
@@ -0,0 +1,5 @@
+document.getElementById('show-gif-button').addEventListener('click', function() {
+    const gifImage = document.getElementById('gif-image');
+    gifImage.style.display = 'block';
+  });
+  
